f1c02ce01fcdf16cc95b4b40b00b16cdc7cbd30a
Johnjesa <95327770+Johnjesa@users.noreply.github.com>
2021-11-30 22:26:12 -0500

    Web Front

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template> 
+  <div class="container">
+    <Navbar/>
+    <router-view class="espacio-contenido"/>
+    <Footer/>
+  </div>  
+</template>
+<script>
+import Navbar from './components/Navbar.vue'
+import Footer from './components/Footer.vue'
+export default {
+  components : {
+    Navbar, Footer
+  }  
+}
+</script>
+<style>
+.espacio-contenido{
+  padding-bottom: 20px;
+  padding-top: 20px;
+}
+</style>
